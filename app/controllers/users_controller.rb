d104ec40802ef86821460fe6c57e4c6e1868ac11
Thomas Countz <Thomascountz@gmail.com>
2017-01-08 02:18:38 +0000

    worst commit message in history

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,16 +19,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-  def show
-    @user = User.find(params[:id])
-  end
-  
   def update
     @user = User.find(params[:id])
-    if @user.attributes = user_params
-      render :edit
+    if @user.update_attributes(user_params)
+      redirect_to edit_user_path(@user)
     else
-      render :show
+      render :edit
     end
   end
   
